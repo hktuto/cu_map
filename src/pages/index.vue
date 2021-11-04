ed7ff6b713de81792043668c86b0a493f38eb581
Seantsang <9394wing@gmail.com>
2021-11-04 16:33:45 +0800

    home page setup

---
+++ src/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mapCntainer">
+    <img class="bg" src="/images/home_bg.svg" />
+    <HomeButton class="bg" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import anime from "animejs";
+export default defineComponent({
+  setup() {
+    const init = () => {
+      //
+    };
+
+    return {};
+  },
+});
+</script>
+<style scoped>
+.bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
